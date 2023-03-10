830720115cd6da3343cf95cc9e4f61400cc3d7ac
kagchi <yonuxbot@gmail.com>
2023-03-10 18:23:26 +0700

    fix: remove unused options

--- src/Typings/index.ts
+++ src/Typings/index.ts
@@ -1,9 +1,7 @@
 import { StoreRegistry } from "@sapphire/pieces";
-import { Awaitable } from "@sapphire/utilities";
 
 /* eslint-disable @typescript-eslint/no-empty-interface */
 export interface ManagerOptions {
-    fetchLanguage?: <T>(identifier: string) => Awaitable<T>;
     baseDirectory?: string;
     stores?: StoreRegistry;
 }
