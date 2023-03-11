401855f624be4e56431cb1f9757ac3e2baced46c
kagchi <yonuxbot@gmail.com>
2023-03-11 19:24:54 +0700

    feat: add few helpers

--- src/Structures/i18nManager.ts
+++ src/Structures/i18nManager.ts
@@ -9,8 +9,23 @@ export class i18nManager {
     public constructor(options: ManagerOptions) {
         this.options = options;
         this.stores = options.stores ??= container.stores;
+        container.i18nManager = this;
 
         this.stores.register(new LanguageStore());
         this.stores.registerPath(this.options.baseDirectory);
+    }
+
+    public get languages(): string[] {
+        return [...new Set(this.stores.get("languages").map(x => x.name.split("/")[0]).map(x => x))];
+    }
+}
+
+declare module "@sapphire/pieces" {
+    interface Container {
+        i18nManager: i18nManager;
+    }
+
+    interface StoreRegistryEntries {
+        languages: LanguageStore;
     }
 }
